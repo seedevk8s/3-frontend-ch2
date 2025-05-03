6c76dea7d18ed3cc90cb19e8d07291f190933225
seedevk8s <seedevk8s@gmail.com>
2025-05-03 19:52:03 +0900

    router 전용 풀더 추가하여 root.tsx에서 해당 라우터 처리하게 분리함

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { RouterProvider } from 'react-router';
+import router from './router/root';
 
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <div>Hello world</div>,
-  },
-]);
+
 
 createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router}></RouterProvider>,
