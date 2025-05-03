79532faa9823071f3f8599b31984d9c0ed7db9b9
seedevk8s <seedevk8s@gmail.com>
2025-05-03 20:40:59 +0900

    <Suspense>와 <Lazy>를 이용한 코드 분할(Code Splitting) 적용

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,8 +1,13 @@
+import { NavLink } from "react-router";
 
 
 function MainPage() {
   return ( 
     <div className="text-3xl">
+      <div className="flex">
+        <NavLink to='/about'>About</NavLink>
+      </div>
+
       <div>Main Page</div>
     </div>
    );
--- src/router/root.tsx
+++ src/router/root.tsx
@@ -4,6 +4,7 @@ import { createBrowserRouter } from "react-router";
 const Loading = () => <div>Loading...</div>;
 
 const Main = lazy(() => import("../pages/mainPage"));
+const About = lazy(() => import("../pages/aboutPage"));
 
 
 const router = createBrowserRouter([
@@ -11,6 +12,10 @@ const router = createBrowserRouter([
     path: "",
     element: <Suspense fallback={<Loading />}><Main /></Suspense>,
   },
+  {
+    path: "about",
+    element: <Suspense fallback={<Loading />}><About /></Suspense>,
+  },  
 ]);
 
 export default router;
