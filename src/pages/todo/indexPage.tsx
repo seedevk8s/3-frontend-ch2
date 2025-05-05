81d98b047a3683b49bd0db56e5d1547d4a6b27d5
seedevk8s <seedevk8s@gmail.com>
2025-05-05 14:16:32 +0900

    Todo폴더내  indexPage.tsx 추가

--- src/pages/todo/indexPage.tsx
+++ src/pages/todo/indexPage.tsx
@@ -3,9 +3,12 @@ import { Outlet } from "react-router";
 
 function IndexPage() {
   return ( 
-    <div className="bg-amber-400 h-9">
-      Todo Index Page
-      <Outlet />
+    <div>
+      <div className="bg-amber-400 h-9">
+        Todo Index Page
+        <Outlet />
+      </div>
+
     </div>
 
    );
