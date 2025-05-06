e175cc7fdd426f26923caf283c8c5db06d37a356
seedevk8s <seedevk8s@gmail.com>
2025-05-06 11:08:24 +0900

    tno값 출력해보기

--- src/pages/todo/readPage.tsx
+++ src/pages/todo/readPage.tsx
@@ -1,5 +1,9 @@
+import { useParams } from "react-router";
 
 function ReadPage() {
+  const obj = useParams();
+  console.log("ReadPage", obj);
+
   return ( 
     <div className="bg-white w-full">
       <div className="text-4xl">Todo Read Page</div>
