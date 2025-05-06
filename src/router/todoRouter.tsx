f7471a132936710a821cb19c21dded6d926db58d
seedevk8s <seedevk8s@gmail.com>
2025-05-06 10:33:57 +0900

    앞서 추가한 페이지를  라우터 적용 추가하기

--- src/router/todoRouter.tsx
+++ src/router/todoRouter.tsx
@@ -3,6 +3,7 @@ import { lazy, Suspense } from "react";
 const Loading = () => <div>Loading...</div>;
 const TodoIndex = lazy(() => import("../pages/todo/indexPage"));
 const TodoList = lazy(() => import("../pages/todo/listPage"));
+const TodoRead = lazy(() => import("../pages/todo/readPage"));
 
 const todoRouter = () => {
 
@@ -14,8 +15,13 @@ const todoRouter = () => {
         {
           path: "list",
           element: <Suspense fallback={<Loading/>}><TodoList /></Suspense>,
-        }
-      ]
+        },
+        {
+          path: "read/:tno",
+          element: <Suspense fallback={<Loading/>}><TodoRead /></Suspense>,
+        }        
+      ],
+
 
     }
   )
